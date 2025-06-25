8e5561346ab12b5a75c47976967a10f78a619e6d
Kersten Kriegbaum <kersten.kriegbaum@digitalfrontiers.de>
2025-06-25 18:30:21 +0200

    draft for new syntax wip

--- framework-test/src/main/java/com/opencqrs/framework/command/Main.java
+++ framework-test/src/main/java/com/opencqrs/framework/command/Main.java
@@ -21,7 +21,7 @@ public class Main {
                 .givenNothing()
                 .when(testCommand)
                 .succeeds()
-                .withNoEvents()
+                .withoutEvents()
                 .havingResult("success")
                 .resultSatisfying(result -> System.out.println("Result: " + result))
                 .havingState(new Object())
@@ -32,36 +32,39 @@ public class Main {
                 
                 .allEvents()
                 .count(3)
-                .single("singleEvent")
+                .single(e -> e.comparing(new Object()))
+//                .single("singleEvent")
                 .singleAsserting(asserter -> asserter.payload("expectedPayload"))
                 .singleType(String.class)
                 .singleSatisfying(event -> System.out.println("Single event: " + event))
+                .exactly().comparing("event1", "event2").ofType(Integer.class).and() // wie verhindern wir, dass jemand das and() weg lässt
                 .exactly("event1", "event2", "event3")
                 .inAnyOrder("eventA", "eventB", "eventC")
                 .expectAnyEvent(asserter -> asserter.payload("anyEvent"))
-                .any("specificEvent")
+                .any().comparing(new Object())
                 .anySatisfying(asserter -> asserter.payloadType(String.class))
                 .anyType(String.class)
-                .none()
+                .none().asserting(asserter -> asserter.payload("forbiddenEvent"))
                 .notContaining(asserter -> asserter.payload("forbiddenEvent"))
                 .notContainingType(Integer.class)
-                .allSatisfying(
+                .all().satisfying(events -> System.out.println("All events: " + events))
+                .allSatisfying( // noch nicht glücklich mit
                     events -> System.out.println("All events: " + events),
                     events -> events.forEach(System.out::println)
                 )
                 
                 .and()
                 
-                .nextEvents()
+                .nextEvents() // besserer Name für nextEvents() ?
                 .skip(2)
                 .andNoMore()
                 .exactly("nextEvent1", "nextEvent2")
                 .matchingTypes(String.class, Integer.class)
                 .inAnyOrder("orderEvent1", "orderEvent2")
                 .matchingTypesInAnyOrder(String.class, Double.class)
-                .comparing("compareEvent")
+                .comparing("compareEvent") // next().comparing()
                 .comparing(asserter -> asserter.payload("comparePayload"))
-                .comparingType(String.class)
+                .comparingType(String.class) //next().ofType()
                 .satisfying(event -> System.out.println("Satisfying: " + event))
                 .any("anyNextEvent")
                 
