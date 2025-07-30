486cd2d5d0ac170805dd5f4e9995e4c11cd4ed50
Michael Graf <michael.graf@digitalfrontiers.de>
2025-07-30 19:00:06 +0200

    refactor: removed JacksonEventCandidate

--- esdb-client/src/main/java/com/opencqrs/esdb/client/jackson/JacksonMarshaller.java
+++ esdb-client/src/main/java/com/opencqrs/esdb/client/jackson/JacksonMarshaller.java
@@ -47,13 +47,11 @@ public class JacksonMarshaller implements Marshaller {
 
     @Override
     public String toWriteEventsRequest(List<EventCandidate> eventCandidates, List<Precondition> preconditions) {
-        var jacksonEventCandidates =
-                eventCandidates.stream().map(this::toJackson).toList();
         var jacksonPreconditions = preconditions.stream().map(this::toJackson).toList();
 
         try {
             return objectMapper.writeValueAsString(
-                    Map.of("events", jacksonEventCandidates, "preconditions", jacksonPreconditions));
+                    Map.of("events", eventCandidates, "preconditions", jacksonPreconditions));
         } catch (JsonProcessingException e) {
             throw new ClientException.MarshallingException(e);
         }
@@ -153,16 +151,6 @@ public class JacksonMarshaller implements Marshaller {
         }
     }
 
-    private JacksonEventCandidate toJackson(EventCandidate eventCandidate) {
-        return new JacksonEventCandidate(
-                eventCandidate.source(),
-                eventCandidate.subject(),
-                eventCandidate.type(),
-                eventCandidate.data(),
-                eventCandidate.traceParent(),
-                eventCandidate.traceState());
-    }
-
     private JacksonPrecondition toJackson(Precondition precondition) {
         return switch (precondition) {
             case Precondition.SubjectIsPristine p ->
@@ -174,15 +162,6 @@ public class JacksonMarshaller implements Marshaller {
                         "isSubjectOnEventId", new JacksonPrecondition.IsOnEventId.Payload(p.subject(), p.eventId()));
         };
     }
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    record JacksonEventCandidate(
-            @NotBlank String source,
-            @NotBlank String subject,
-            @NotBlank String type,
-            @NotNull Map<String, ?> data,
-            String traceparent,
-            String tracestate) {}
 
     interface JacksonPrecondition {
         record IsPristine(String type, Payload payload) implements JacksonPrecondition {
