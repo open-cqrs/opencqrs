e8d5241bd2faaa408dc4dada07211b24ebb779bf
Erich Wittenbeck <erich.wittenbeck@digitalfrontiers.de>
2025-08-01 15:38:40 +0200

    fix: add missing autoconfig import and order config precedence

--- esdb-client-spring-boot-autoconfigure/src/main/java/com/opencqrs/esdb/client/tracing/NoTracingEventEnricherAutoConfiguration.java
+++ esdb-client-spring-boot-autoconfigure/src/main/java/com/opencqrs/esdb/client/tracing/NoTracingEventEnricherAutoConfiguration.java
@@ -2,10 +2,13 @@
 package com.opencqrs.esdb.client.tracing;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 
-@AutoConfiguration(after = OpenTelemetryEventEnricherAutoConfiguration.class)
+@AutoConfiguration
+@AutoConfigureOrder(Ordered.LOWEST_PRECEDENCE)
 public class NoTracingEventEnricherAutoConfiguration {
 
     @Bean
