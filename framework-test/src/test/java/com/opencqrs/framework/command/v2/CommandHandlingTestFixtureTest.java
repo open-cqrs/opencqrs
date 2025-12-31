7cf1356b3466b5f5e41af939f4ce43a5bf0d0ba9
Kersten Kriegbaum <kersten.kriegbaum@digitalfrontiers.de>
2025-12-31 12:03:10 +0100

    start implementing tests for test fixture v2

--- framework-test/src/test/java/com/opencqrs/framework/command/v2/CommandHandlingTestFixtureTest.java
+++ framework-test/src/test/java/com/opencqrs/framework/command/v2/CommandHandlingTestFixtureTest.java
@@ -1,31 +1,114 @@
 /* Copyright (C) 2025 OpenCQRS and contributors */
 package com.opencqrs.framework.command.v2;
 
-import com.opencqrs.esdb.client.Event;
 import com.opencqrs.framework.command.*;
-import com.opencqrs.framework.command.CommandHandlingTestFixture;
-import com.opencqrs.framework.persistence.CapturedEvent;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.Serializable;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verifyNoInteractions;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CommandHandlingTestFixtureTest {
 
+    @Nested
+    public class Setup {
+
+        @Test
+        public void stateRebuildingHandlersCalledFilteredByInstanceClassBeforeCommandExecution() {
+            StateRebuildingHandlerDefinition[] eshds = {
+                    new StateRebuildingHandlerDefinition(
+                            State.class, EventA.class, (StateRebuildingHandler.FromObject) (i, e) -> {
+                                throw new AssertionError("wrong state rebuilding handler called");
+                    }),
+                    new StateRebuildingHandlerDefinition(
+                            AnotherState.class, EventA.class, (StateRebuildingHandler.FromObject)
+                                (i, e) -> new AnotherState())
+            };
+
+            CommandHandlingTestFixture.withStateRebuildingHandlerDefinitions(eshds)
+                    .using(new CommandHandlerDefinition(
+                            AnotherState.class,
+                            DummyCommand.class,
+                            (CommandHandler.ForInstanceAndCommand<AnotherState, DummyCommand, Void>) (i, c, p) -> {
+                                assertThat(i).isNotNull();
+                                return null;
+                            }))
+                    .given()
+                    .event(e -> e.payload(new EventA("irrelevant")))
+                    .when(new DummyCommand())
+                    .succeeds();
+        }
+
+        @Test
+        public void stateRebuildingHandlersCalledFilteredByEventClassAfterCommandExecution() {
+            StateRebuildingHandlerDefinition[] eshds = {
+                    new StateRebuildingHandlerDefinition(
+                            State.class, EventA.class, (StateRebuildingHandler.FromObject) (i, e) -> {
+                        throw new AssertionError("wrong state rebuilding handler called");
+                    }),
+                    new StateRebuildingHandlerDefinition(
+                            AnotherState.class, EventA.class, (StateRebuildingHandler.FromObject)
+                            (i, e) -> new AnotherState())
+            };
+
+            CommandHandlingTestFixture.withStateRebuildingHandlerDefinitions(eshds)
+                    .using(new CommandHandlerDefinition(
+                            AnotherState.class,
+                            DummyCommand.class,
+                            (CommandHandler.ForInstanceAndCommand<AnotherState, DummyCommand, Void>) (i, c, p) -> {
+                                assertThat(i).isNull();
+                                p.publish(new EventA("irrelevant"));
+                                return null;
+                            }))
+                    .given()
+                    .nothing()
+                    .when(new DummyCommand())
+                    .succeeds()
+                    .then()
+                    .allEvents()
+                    .exactly(event -> event.ofType(EventA.class));
+        }
+    }
+
+    static class DummyCommand implements Command {
+
+        private final SubjectCondition subjectCondition;
+
+        DummyCommand() {
+            this(SubjectCondition.NONE);
+        }
+
+        DummyCommand(SubjectCondition subjectCondition) {
+            this.subjectCondition = subjectCondition;
+        }
+
+        @Override
+        public String getSubject() {
+            return "dummy";
+        }
+
+        @Override
+        public SubjectCondition getSubjectCondition() {
+            return subjectCondition;
+        }
+    }
+
+    record State(Boolean valid) {}
+
+    record AnotherState() {}
+
+    record EventA(String name) implements Serializable {}
+
+    record EventB(Long size) {}
+
+    record EventC() {}
+
+    private static <E> StateRebuildingHandlerDefinition<State, E> eshIdentity(Class<E> eventClass) {
+        return new StateRebuildingHandlerDefinition<>(
+                State.class, eventClass, (StateRebuildingHandler.FromObject<State, E>)
+                (instance, event) -> Optional.ofNullable(instance).orElse(new State(true)));
+    }
 
 }
