feebc622c958c3e1c4f274e49e6fa4823b0ea26f
Frank Scheffler <frank.scheffler@digitalfrontiers.de>
2026-07-13 07:01:02 +0200

    fix: avoid child context creation for event handling processors

--- framework-spring-boot-autoconfigure/src/main/java/com/opencqrs/framework/eventhandler/LeaderElectionEventHandlingProcessorLifecycleController.java
+++ framework-spring-boot-autoconfigure/src/main/java/com/opencqrs/framework/eventhandler/LeaderElectionEventHandlingProcessorLifecycleController.java
@@ -10,18 +10,18 @@ import org.springframework.integration.leader.AbstractCandidate;
 import org.springframework.integration.leader.Context;
 
 /**
- * {@link EventHandlingProcessorLifecycleController} implementation which in turn is managed by a
+ * Life-cycle implementation for the {@link EventHandlingProcessor} which in turn is managed by a
  * {@link org.springframework.integration.support.leader.LockRegistryLeaderInitiator} bean using {link
  * {@link #onGranted(Context)}} and {@link #onRevoked(Context)}.
+ *
+ * @see EventHandlingProcessorAutoConfiguration#openCqrsLeaderElectionEventHandlingProcessorLifecycleControllerFactory()
  */
-class LeaderElectionEventHandlingProcessorLifecycleController extends AbstractCandidate
-        implements EventHandlingProcessorLifecycleController {
+class LeaderElectionEventHandlingProcessorLifecycleController extends AbstractCandidate {
 
     private static final Logger log =
             Logger.getLogger(LeaderElectionEventHandlingProcessorLifecycleController.class.getName());
 
     private final EventHandlingProcessor eventHandlingProcessor;
-    private boolean running = false;
 
     LeaderElectionEventHandlingProcessorLifecycleController(EventHandlingProcessor eventHandlingProcessor) {
         super(
@@ -37,13 +37,11 @@ class LeaderElectionEventHandlingProcessorLifecycleController extends AbstractCa
         CompletableFuture.runAsync(() -> {
             try {
                 Future<?> started = eventHandlingProcessor.start();
-                running = true;
                 started.get();
             } catch (ExecutionException | InterruptedException e) {
                 log.log(Level.INFO, eventHandlingProcessor.eventProcessorForLogs() + " prematurely terminated", e);
             } finally {
                 log.info(() -> eventHandlingProcessor.eventProcessorForLogs() + " yielding leadership: " + ctx);
-                running = false;
                 ctx.yield();
             }
         });
@@ -53,12 +51,6 @@ class LeaderElectionEventHandlingProcessorLifecycleController extends AbstractCa
     public void onRevoked(Context ctx) {
         log.warning(() -> "leadership revoked for " + eventHandlingProcessor.eventProcessorForLogs() + ": " + ctx);
         eventHandlingProcessor.stop();
-        running = false;
         ctx.yield();
-    }
-
-    @Override
-    public boolean isRunning() {
-        return running;
     }
 }
